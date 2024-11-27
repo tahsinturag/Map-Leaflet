a12aa6e9d08493f6897fbeae7017beb518160fda
tahsinturag <tahsinturag10@gmail.com>
2024-11-27 12:46:38 +0600

    search form added (reactive approach)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LeafletMapComponent} from './leaflet-map/leaflet-map.component';
+import {SearchFormComponent} from './search-form/search-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LeafletMapComponent],
+  imports: [RouterOutlet, LeafletMapComponent, SearchFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
