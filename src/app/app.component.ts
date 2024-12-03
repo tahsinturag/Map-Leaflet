6132a4aeb124f8ef2e6cdf98f2e8363b8fe56fe0
tahsinturag <tahsinturag10@gmail.com>
2024-12-03 13:34:24 +0600

    http request for sylhet (GET)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LeafletMapComponent} from './leaflet-map/leaflet-map.component';
 import {SearchFormComponent} from './search-form/search-form.component';
 import {SignInComponent} from './sign-in/sign-in.component';
 import {DhakaComponent} from './dhaka/dhaka.component';
+import {PostService} from './post.service';
+import {FormBuilder} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 
 @Component({
@@ -13,8 +16,32 @@ import {DhakaComponent} from './dhaka/dhaka.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+// interface Post {
+//   useId:number;
+//   id: number;
+//   title: string;
+//   body: string;
+// }
 export class AppComponent {
   title = 'bangladesh';
+  postService = inject (PostService);
+  // posts:Post[]=[];
+  posts:any =[];
+  constructor() {
+    this.postService.getPosts().subscribe({
+      next: (data: any)=> {
+        this.posts = data;
+      },
+      error: (err:any)=>{
+        console.log(err);
+      }
+    });
+  }
+
+
+
+
 
 
 
