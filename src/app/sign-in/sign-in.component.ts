24cac80abfd44e7de9980f66b9eb8fb6ef4362a8
tahsinturag <tahsinturag10@gmail.com>
2024-12-09 12:14:42 +0600

    previous issue solved

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,57 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import {
-  ReactiveFormsModule,
-  FormBuilder,
+  FormControl,
   FormGroup,
-  Validators
+  ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrl: './sign-in.component.css',
 })
-export class SignInComponent implements OnInit {
-  regForm: any;
+export class SignInComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.regForm = this.fb.group({
-      userName: ['', [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(8)
-      ]],
-      email: ['', [
-        Validators.required,
-        Validators.email
-      ]],
-      password: ['', [
-        Validators.required,
-        Validators.minLength(6)
-      ]]
-    });
+  get emailIsInvalid() {
+    const email = this.form.get('email');
+    return email?.touched && email.dirty && email.invalid;
   }
 
-  signIn() {
-    if (this.regForm.valid) {
-      console.log(this.regForm.value);
+  get passwordIsInvalid() {
+    const password = this.form.get('password');
+    return password?.touched && password.dirty && password.invalid;
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      console.log('Email:', email, 'Password:', password);
     } else {
-      this.markFormGroupTouched(this.regForm);
+      console.log('Form is invalid');
     }
-  }
-
-  markFormGroupTouched(formGroup: FormGroup) {
-    Object.values(formGroup.controls).forEach(control => {
-      control.markAsTouched();
-
-      if (control instanceof FormGroup) {
-        this.markFormGroupTouched(control);
-      }
-    });
   }
 }
