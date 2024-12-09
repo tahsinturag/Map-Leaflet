c250ea4a6ddde68b92aeac68f20b0ed753b69cae
tahsinturag <tahsinturag10@gmail.com>
2024-12-09 13:23:35 +0600

    previous another issue solved

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,33 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+// Add vendor suggestion interface
+interface VendorSuggestion {
+  id: number;
+  vendorName: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
+
+
 export class PostService {
-
-
-
   // Base URLs for API endpoints
-  // private apiUrl = 'http://192.168.0.172:8083/api/divisions/geojson?name=';
   private apiUrl = 'http://192.168.0.172:8083/api/division/boundary?name=';
 
   private genericUrl = 'http://192.168.0.172:8083/api/generic/search?prefix=';
 
-  private demographicUrl = 'http://192.168.0.109:8083/api/sales-info/demographic';
-  // API endpoint for fetching demographic data.
-  constructor(private http: HttpClient) {}
+  // need to work with this
+  private vendorUrl = 'http://192.168.0.172:8083/api/vendor/search?prefix=';
 
+
+  private demographicUrl = 'http://192.168.0.109:8083/api/sales-info/demographic';
+
+  constructor(private http: HttpClient) {}
+  //for boundary region
   getPosts(name: string): Observable<any> {
     const apiUrl = `${this.apiUrl}${name}`;
     return this.http.get(apiUrl);
   }
-
+  // for genericSuggestions
   getGenericSuggestions(query: string): Observable<any> {
     const url = `${this.genericUrl}${query}&limit=10`;
     return this.http.get(url);
   }
-
+  // for vendor suggestions
+  getVendorSuggestions(query: string): Observable<VendorSuggestion[]> {
+    const url = `${this.vendorUrl}${query}&limit=10`;
+    return this.http.get<VendorSuggestion[]>(url);
+  }
+  // for demographic
   getDemographicData(
     genericId: number,
     vendorId: number,
--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -3,9 +3,16 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { PostService } from '../post.service';
 import { CommonModule } from '@angular/common';
 
+// GenericSuggestion interface
 interface GenericSuggestion {
   id: number;
   genericName: string;
+}
+
+// VendorSuggestion interface
+interface VendorSuggestion {
+  id: number;
+  vendorName: string;
 }
 
 @Component({
@@ -21,8 +28,12 @@ export class SearchFormComponent implements OnInit {
   @Output() closeModalEmitter = new EventEmitter<void>();
   @Output() searchParamEmitter = new EventEmitter<any>();
   public searchForm!: FormGroup;
+
+  // vendor and generic suggestions property
   public suggestions: GenericSuggestion[] = [];
+  public vendorSuggestions: VendorSuggestion[] = [];
   showSuggestion: boolean = false;
+  showVendorSuggestion: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -31,13 +42,24 @@ export class SearchFormComponent implements OnInit {
 
   ngOnInit() {
     this.initializeSearchForm();
-    //  for fetching suggestions from api
+
+    // generic suggestions logic
     this.searchForm.get('brand_name')?.valueChanges.subscribe((input) => {
       if (input && input.length > 0) {
         this.getSuggestions(input);
       } else {
         this.suggestions = [];
         this.showSuggestion = false;
+      }
+    });
+
+    // vendor suggestions logic
+    this.searchForm.get('vendor_name')?.valueChanges.subscribe((input) => {
+      if (input && input.length > 0) {
+        this.getVendorSuggestions(input);
+      } else {
+        this.vendorSuggestions = [];
+        this.showVendorSuggestion = false;
       }
     });
   }
@@ -51,6 +73,7 @@ export class SearchFormComponent implements OnInit {
     });
   }
 
+  // get generic suggestions method
   getSuggestions(query: string) {
     this.postService.getGenericSuggestions(query).subscribe((response: GenericSuggestion[]) => {
       this.suggestions = response;
@@ -58,17 +81,39 @@ export class SearchFormComponent implements OnInit {
     });
   }
 
+  // get vendor suggestions method
+  getVendorSuggestions(query: string) {
+    this.postService.getVendorSuggestions(query).subscribe((response: VendorSuggestion[]) => {
+      this.vendorSuggestions = response;
+      this.showVendorSuggestion = true;
+    });
+  }
+
+  // generic suggestion selection method
   selectSuggestion(suggestion: GenericSuggestion) {
     this.searchForm.get('brand_name')?.setValue(suggestion.genericName);
     this.suggestions = [];
-    this.showSuggestion = false; // Hide dropdown
+    this.showSuggestion = false;
   }
 
-  onBlur() {
-    setTimeout(() => {
-      this.showSuggestion = false; // Hide dropdown after a short delay
-    }, 200); // Delay to ensure clicks are processed
+  // vendor suggestion selection method
+  selectVendorSuggestion(suggestion: VendorSuggestion) {
+    this.searchForm.get('vendor_name')?.setValue(suggestion.vendorName);
+    this.vendorSuggestions = [];
+    this.showVendorSuggestion = false;
   }
+
+  onBlur(type: 'generic' | 'vendor') {
+    setTimeout(() => {
+      if (type === 'generic') {
+        this.showSuggestion = false;
+      } else {
+        this.showVendorSuggestion = false;
+      }
+    }, 200);
+  }
+
+
 
   searchHandler() {
     if (this.searchForm.valid) {
