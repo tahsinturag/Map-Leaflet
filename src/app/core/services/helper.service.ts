5daa1b8230013181542f0fe1654b4a5e41992d9c
tahsinturag <tahsinturag10@gmail.com>
2024-12-10 17:22:34 +0600

    some issues fixed

--- src/app/core/services/helper.service.ts
+++ src/app/core/services/helper.service.ts
@@ -4,16 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class HelperService {
-  private tabType: string = 'tabType';
-  constructor() {}
-  setTabItem(tabType: string) {
-    localStorage.setItem(this.tabType, JSON.stringify(tabType));
-  }
-  getTabItem() {
-    const data: any = localStorage.getItem(this.tabType);
-    return JSON.parse(data) || '';
-  }
-  removeTabItem() {
-    localStorage.removeItem(this.tabType);
-  }
+  // private tabType: string = 'tabType';
+  // constructor() {}
+  // setTabItem(tabType: string) {
+  //   localStorage.setItem(this.tabType, JSON.stringify(tabType));
+  // }
+  // getTabItem() {
+  //   const data: any = localStorage.getItem(this.tabType);
+  //   return JSON.parse(data) || '';
+  // }
+  // removeTabItem() {
+  //   localStorage.removeItem(this.tabType);
+  // }
 }
--- src/app/leaflet-map/map.routes.ts
+++ src/app/leaflet-map/map.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LeafletMapComponent } from '../leaflet-map/leaflet-map.component';
-// import { LeafletMapComponent } from './containers/leaflet-map-container/leaflet-map-container.component';
 
 export const mapRoutes: Routes = [
   {
