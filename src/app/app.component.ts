d4f53243a5a991dd7b3618133c340f5436cd16bd
tahsinturag <tahsinturag10@gmail.com>
2024-12-11 12:51:13 +0600

    issues fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,6 @@ import {LeafletMapComponent} from './leaflet-map/leaflet-map.component';
 import {SearchFormComponent} from './search-form/search-form.component';
 import {SignInComponent} from './sign-in/sign-in.component';
 import {PostService} from './post.service';
-import {FormBuilder} from '@angular/forms';
-import {CommonModule} from '@angular/common';
 import {ShowResultsComponent} from './show-results/show-results.component';
 
 
@@ -17,33 +15,12 @@ import {ShowResultsComponent} from './show-results/show-results.component';
   styleUrl: './app.component.css'
 })
 
-// interface Post {
-//   useId:number;
-//   id: number;
-//   title: string;
-//   body: string;
-// }
+
 export class AppComponent {
   title = 'bangladesh';
   postService = inject (PostService);
-  // posts:Post[]=[];
   posts:any =[];
   constructor() {
-    // this.postService.getPosts().subscribe({
-    //   next: (data: any)=> {
-    //     console.log(data.geoData?.features[0].geometry.coordinates)
-    //     this.posts = data.geoData?.features[0].geometry.coordinates;
-    //   },
-    //   error: (err:any)=>{
-    //     console.log(err);
-    //   }
-    // });
+
   }
-
-
-
-
-
-
-
 }
